97b58af80b8784e5ce4500b97c5a5ca8e9504ddc
Vugar Mammadli <mammadli.vugar@hotmail.com>
2024-12-12 20:00:41 +0100

    first init

---
+++ p01_compressor/target/debug/deps/p01_compressor-a2f354a9c21a5104.d
@@ -0,0 +1,5 @@
+/home/vuqar/RustroverProjects/rust_projects/p01_compressor/target/debug/deps/p01_compressor-a2f354a9c21a5104: src/main.rs
+
+/home/vuqar/RustroverProjects/rust_projects/p01_compressor/target/debug/deps/p01_compressor-a2f354a9c21a5104.d: src/main.rs
+
+src/main.rs:
---
+++ p01_compressor/target/debug/p01_compressor.d
@@ -0,0 +1,1 @@
+/home/vuqar/RustroverProjects/rust_projects/p01_compressor/target/debug/p01_compressor: /home/vuqar/RustroverProjects/rust_projects/p01_compressor/src/main.rs

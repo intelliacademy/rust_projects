32dde5633b4c6a1d31a20f860f150a9bc8e26e81
Vugar Mammadli <mammadli.vugar@hotmail.com>
2024-12-13 15:45:26 +0100

    port_sniffer_cli

--- src/c2_port_sniffer_cli.rs
+++ src/c2_port_sniffer_cli.rs
@@ -1,6 +1,11 @@
 use std::env::args;
 use std::net::IpAddr;
+use std::{io, process};
+use std::io::Write;
 use std::str::FromStr;
+use std::sync::mpsc::{channel, Sender};
+
+const MAX: u16 = 65535;
 
 pub fn proceed() {
     println!("Port sniffer CLI");
@@ -10,15 +15,65 @@ pub fn proceed() {
     for arg in args.iter() {
         println!("Argument {:?}", arg);
     }
+    println!("---------------------------------");
     let mut arguments = Arguments::new(args.clone()).unwrap_or_else(
         |err| {
-            println!("Problem parsing arguments: {}", err);
-            std::process::exit(1);
+            if err.contains("help") {
+                process::exit(0);
+            }else {
+                eprintln!("Problem parsing arguments: {}", err);
+                eprintln!("Ip address format [1]: {:?} \
+                \nThread number format [2]: {:?}", args[1], args[2]);
+                process::exit(0);
+            }
         }
     );
 
+    let mut threads = arguments.threads;
+    let (tx,rx) = channel();
+    for i in 0..threads {
+        let tx = tx.clone();
+        std::thread::spawn(move || {
+            scan(tx, i, arguments.ip, threads);
+        });
+    }
+
+    let mut out = vec![];
+    drop(tx);
+
+    for p in rx {
+        out.push(p);
+    }
+
+    println!("---------------------------------");
+
+    out.sort();
+
+    for v in out {
+        println!("Port {} is open", v);
+    }
+
 }
 
+fn scan(tx: Sender<u16>, p1: u16, ip_addr: IpAddr, p3: u16) {
+    println!("Thread {} scanning", p1);
+    let mut port:u16 = p1 + 1;
+    loop {
+        match std::net::TcpStream::connect((ip_addr, port)) {
+            Ok(_) => {
+                io::stdout().flush().unwrap();
+                tx.send(port.clone()).expect("Could not send data");
+            }
+            Err(_) => {}
+        }
+        if (MAX - port) <= p3 {
+            break;
+        }
+        port += p3;
+    }
+}
+
+#[derive(Debug)]
 struct Arguments {
     flag: String,
     ip: IpAddr,
@@ -35,8 +90,9 @@ impl Arguments {
         }
 
         let ip = args[1].clone();
-        return if let Ok(ipx) = IpAddr::from_str(&ip) {
-            Err("Invalid IP address")
+        if let Ok(ipx) = IpAddr::from_str(&ip) {
+            let threads = args[2].parse::<u16>().unwrap_or_else(|_| 16_u16);
+            Ok(Arguments { flag: String::from("scan"), ip: ipx, threads })
         } else {
             let flag = args[1].clone();
             if flag != "-h" && flag != "-help" && flag != "-t" {
